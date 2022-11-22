8b22e9d0a9d7a4a6c4b41dc3e7828fe099e04036
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-22 12:23:29 +0200

    react router

---
+++ react/11_react_router_test/src/components/Home.js
@@ -0,0 +1,8 @@
+const Home = (props) => {
+
+	return(
+		<h2>Welcome to router test</h2>
+	)
+}
+
+export default Home;
---
+++ react/11_react_router_test/src/components/Secret.js
@@ -0,0 +1,9 @@
+const Secret = (props) => {
+	
+	return(
+		<h2>Welcome to the secret page</h2>
+	)
+
+}
+
+export default Secret;
