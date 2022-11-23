1ad249c07ca0523415aa296fe562f853b5354ae2
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-23 14:26:14 +0200

    shopping app login backend

--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const apiroute = require("./routes/apiroute");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 let app = express();
 
@@ -7,6 +9,45 @@ app.use(express.json());
 
 //PORT
 let port = process.env.PORT || 3001;
+
+//LOGIN DATABASES
+
+let registeredUsers = [];
+let loggedSessions = [];
+let time_to_live_diff = 3600000;
+
+//MIDDLEWARE
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({message:"Bad request"});
+	}
+	for(let i=0;i<registeredUsers.length;i++) {
+		if(req.body.username === registeredUsers[i].username) {
+			return res.status(409).json({message:"Username is already in use"})
+		}
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			return res.status(500).json({message:"Internal server error"})
+		}
+		let user = {
+			"username":req.body.username,
+			"password":hash
+		}
+		registeredUsers.push(user);
+		console.log(user);
+		return res.status(200).json({message:"Register success!"});
+	})
+})
 
 app.use("/api",apiroute);
 
