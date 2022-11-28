4920a3562b3d3b47b19eff52692d6c2c99975135
Erno Hentonen <erno.hentonen@opiframe.com>
2022-11-28 12:15:27 +0200

    shopping items

--- react/50_shopping_app/03_stage_mongodb/backend/routes/apiroute.js
+++ react/50_shopping_app/03_stage_mongodb/backend/routes/apiroute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const itemModel = require("../models/item");
 
 let router = express.Router();
 
@@ -7,50 +8,68 @@ let database = [];
 let id = 100;
 
 router.get("/shopping",function(req,res) {
-	let tempDatabase = database.filter(item => item.user === req.session.user)
-	return res.status(200).json(tempDatabase);
+	let query = {"user":req.session.user}
+	itemModel.find(query,function(err,items) {
+		if(err) {
+			console.log("Item query failed. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
+		}
+		return res.status(200).json(items);
+	})
 });
 
 router.post("/shopping",function(req,res) {
-	let item = {
-		id:id,
+	if(!req.body) {
+		return res.status(400).json({message:"Bad request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({message:"Bad request"})
+	}
+	let item = new itemModel({
 		type:req.body.type,
 		count:req.body.count,
 		price:req.body.price,
 		user:req.session.user
-	}
-	database.push(item);
-	id++;
-	return res.status(201).json(item);
+	})
+	item.save(function(err) {
+		if(err) {
+			console.log("Failed to create new item. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
+		}
+		return res.status(201).json({message:"Created"});
+	})
 });
 
 router.delete("/shopping/:id",function(req,res) {
-	let id = parseInt(req.params.id,10);
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === id && database[i].user === req.session.user) {
-			database.splice(i,1);
-			return res.status(200).json({message:"Success"});
+	itemModel.deleteOne({"_id":req.params.id,"user":req.session.user},function(err) {
+		if(err) {
+			console.log("Failed to remove item. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"Not found"});
+		return res.status(200).json({message:"Success"})
+	})
 })
 
 router.put("/shopping/:id",function(req,res) {
-	let id = parseInt(req.params.id,10);
+	if(!req.body) {
+		return res.status(400).json({message:"Bad request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({message:"Bad request"})
+	}
 	let item = {
-		id:id,
 		type:req.body.type,
 		count:req.body.count,
 		price:req.body.price,
 		user:req.session.user
 	}
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === id && database[i].user === req.session.user) {
-			database.splice(i,1,item);
-			return res.status(200).json({message:"Success"});
+	itemModel.replaceOne({"_id":req.params.id,"user":req.session.user},item,function(err) {
+		if(err) {
+			console.log("Failed to update item. Reason",err);
+			return res.status(500).json({message:"Internal server error"})
 		}
-	}
-	return res.status(404).json({message:"Not found"});	
+		return res.status(200).json({message:"Success"});
+	})
 })
 
 module.exports = router;
