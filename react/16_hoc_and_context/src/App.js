cd9a874fe6260f716d0d88ad8455864c74838b7e
Erno Hentonen <erno.hentonen@opiframe.com>
2022-12-01 13:24:20 +0200

    hoc and context¨

--- react/16_hoc_and_context/src/App.js
+++ react/16_hoc_and_context/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
 	<div className="App">
 		<ContactForm/>
+		<hr/>
+		<ContactList/>
     </div>
   );
 }
--- react/16_hoc_and_context/src/components/ContactList.js
+++ react/16_hoc_and_context/src/components/ContactList.js
@@ -1,32 +1,37 @@
-const ContactList = (props) => {
+import React from 'react';
+import ContactHandler from '../context/ContactHandler';
 
-	let contacts = props.list.map((contact) => {
-		return(
-			<tr key={contact.id}>
-				<td>{contact.firstname}</td>
-				<td>{contact.lastname}</td>
-				<td>{contact.email}</td>
-				<td>{contact.phone}</td>
-				<td><button onClick={() => props.removeContact(contact.id)}>Remove</button></td>
-			</tr>
-		)
-	})
-	return(
-		<table>
-			<thead>
-				<tr>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Email</th>
-					<th>Phone</th>
-					<th>Remove</th>
+class ContactList extends React.Component {
+
+	render() {
+		let contacts = this.props.list.map((contact) => {
+			return(
+				<tr key={contact.id}>
+					<td>{contact.firstname}</td>
+					<td>{contact.lastname}</td>
+					<td>{contact.email}</td>
+					<td>{contact.phone}</td>
+					<td><button onClick={() => this.props.removeContact(contact.id)}>Remove</button></td>
 				</tr>
-			</thead>
-			<tbody>
-			{contacts}
-			</tbody>
-		</table>
-	)
+			)
+		})
+		return(
+			<table>
+				<thead>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Remove</th>
+					</tr>
+				</thead>
+				<tbody>
+				{contacts}
+				</tbody>
+			</table>
+		)
+	}
 }
 
-export default ContactList;
+export default ContactHandler(ContactList)
