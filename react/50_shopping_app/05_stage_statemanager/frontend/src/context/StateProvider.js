0cded08f0c29fcbf10a509020c226259ba755ea4
Erno Hentonen <erno.hentonen@opiframe.com>
2022-12-01 09:59:30 +0200

    context

---
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/context/StateProvider.js
@@ -0,0 +1,44 @@
+import {useReducer} from 'react';
+import AppStateContext from './AppStateContext';
+import ActionContext from './ActionContext';
+import * as actionConstants from '../types/actionConstants';
+
+const getInitialState = () => {
+	if(sessionStorage.getItem("state")) {
+		let state = JSON.parse(sessionStorage.getItem("state"));
+		return state;
+	} else {
+		return {
+			list:[],
+			isLogged:false,
+			loading:false,
+			error:"",
+			token:""
+		}
+	}
+}
+
+const initialState = getInitialState();
+
+const saveToStorage = (state) => {
+	sessionStorage.setItem("state",JSON.stringify(state));
+}
+
+const listReducer = (state,action) => {
+	
+}
+
+const StateProvider = (props) => {
+	
+	const [state,dispatch] = useReducer(listReducer,initialState);
+
+	return(
+		<AppStateContext.Provider value={state}>
+			<ActionContext.Provider value={{dispatch:dispatch}}>
+				{props.children}
+			</ActionContext.Provider>
+		</AppStateContext.Provider>
+	)
+}
+
+export default StateProvider;
