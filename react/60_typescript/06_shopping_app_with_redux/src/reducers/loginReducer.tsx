22db16d2222f90c2756b3dd3f5ca1a4083b8a435
Erno Hentonen <erno.hentonen@opiframe.com>
2022-12-07 10:00:03 +0200

    shoppingapp typescript

---
+++ react/60_typescript/06_shopping_app_with_redux/src/reducers/loginReducer.tsx
@@ -0,0 +1,89 @@
+import * as actionConstants from '../types/actionConstants';
+import {LoginState} from '../types/states';
+import {AnyAction,Reducer} from 'redux';
+
+const getInitialState = ():LoginState => {
+	let state = sessionStorage.getItem("loginstate");
+	if(state) {
+		return JSON.parse(state);
+	} else {
+		return {
+			loading:false,
+			isLogged:false,
+			token:"",
+			error:""
+		}
+	}
+}
+
+const saveToStorage = (state:LoginState) => {
+	sessionStorage.setItem("loginstate",JSON.stringify(state));
+}
+
+const initialState:LoginState = getInitialState();
+
+const loginReducer:Reducer<LoginState,AnyAction> = (state:LoginState = initialState,action:AnyAction):LoginState => {
+	console.log("LoginReducer, Action",action);
+	let tempState:LoginState = {
+		...state
+	}
+	switch(action.type) {
+		case actionConstants.LOADING:
+			return {
+				...state,
+				loading:true,
+				error:""
+			}
+		case actionConstants.STOP_LOADING:
+			return {
+				...state,
+				loading:false
+			}
+		case actionConstants.REGISTER_SUCCESS:
+			tempState = {
+				...state,
+				error:"Register success!"
+			}
+			saveToStorage(tempState);
+			return;
+		case actionConstants.LOGIN_SUCCESS:
+			tempState = {
+				isLogged:true,
+				error:"",
+				token:action.token,
+				loading:false
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.REGISTER_FAILED:
+		case actionConstants.LOGIN_FAILED:
+			tempState = {
+				...state,
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGOUT_SUCCESS:
+			tempState = {
+				isLogged:false,
+				loading:false,
+				error:"",
+				token:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGOUT_FAILED:
+			tempState = {
+				isLogged:false,
+				loading:false,
+				token:"",
+				error:action.error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		default:
+			return state;
+	}
+}
+
+export default loginReducer;
---
+++ react/60_typescript/06_shopping_app_with_redux/src/types/states.tsx
@@ -0,0 +1,19 @@
+import ShoppingItem from '../models/ShoppingItem';
+
+
+export interface LoginState {
+	isLogged:boolean;
+	token:string;
+	loading:boolean;
+	error:string;
+}
+
+export interface ShoppingState {
+	list:ShoppingItem[];
+	error:string;
+}
+
+export interface AppState {
+	login:LoginState;
+	shopping:ShoppingState;
+}
